2bf844d18c9d98076717e6357bd061635b03eca8
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-01-17 17:42:23 -0300

    Improve URL format

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  scope "(:locale)", locale: /en|es/ do
+    root 'home#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
