4b2ef9e6794b37cd96a5fa8f1834f1014294fdb4
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-01-22 11:52:13 -0300

    Add Contact section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def thanks
+    flash[:notice] = t('contact.thanks')
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|es/ do
     root 'home#index'
+    get :thanks, to: 'home#thanks'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
